229042b6963953e39e06702d64630717d6113c7c
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2020-10-02 15:02:10 +0600

    Update phoneBookController.js

--- BackEnd/src/controllers/phoneBookController.js
+++ BackEnd/src/controllers/phoneBookController.js
@@ -3,76 +3,112 @@ const Validator = require('../../config/validation');
 const User = require('../models/phoneBookModel');
 
  
-// Find All 
-const getAllUser = (req, res) => {
-    User.find()
-        .then(result => {
-            return res.status(200).json({
-                message: "Get All User.",
-                Profile: result
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: "Error, Please try again.", 
-                error: err.message,
-            });
+// Find All User
+async function getAllUser(req, res) {
+    try {
+      const result = await User.find();
+      if (result) {
+        return res.status(200).json({
+          message: "Get All User.",
+          Profile: result,
         });
+      } 
+    }
+    catch (err) {
+      res.status(400).json(err);
+    }
 };
 
 
 // Find user by mobile number
-const getUserByMobileNumber = (req, res) => {
-  const validation = Joi.object(Validator.mobileValidation);
-  const { error, value } = validation.validate(req.params, Validator.options);
+async function getUserByMobileNumber(req, res) {
+    try {
+        // validation check
+        const validation = Joi.object(Validator.mobileValidation);
+        const { error, value } = validation.validate(req.params, Validator.options);
 
-  let LastEleven = value.mobileNumber.substring(value.mobileNumber.length - 11);
-  //console.log('LastEleven = ', LastEleven);
-  if (error) res.json({ message: "Validation Error" });
-  else {
-    User.findOne({ mobileNumber: LastEleven })
-        .then((result) => {
+        let LastEleven = value.mobileNumber.substring(value.mobileNumber.length - 11);
+
+        if (error) res.json({ message: "Validation Error" });
+        else {
+            const result = await User.findOne({ mobileNumber: LastEleven })
             if (result) {
-              return res.status(200).json({
-                message: "User Profile.",
-                Profile: result,
-              });
+                return res.status(200).json({
+                  message: "User Profile.",
+                  Profile: result,
+                });
             }
-            return res.status(202).json({
-              message: "Contact not found!"
-            }); 
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "Error, Please try again.",
-          error: err.message
-        });
-      });
-  }
+            else {
+                return res.status(202).json({
+                    message: "Contact not found!",
+                });
+            }
+        }
+    }
+    catch (err) {
+      res.status(400).json(err);
+    }
 };
-
+ 
 
 // Create User 
 async function createUser(req, res) {
     try {
+        // validation check
         const validation = Joi.object(Validator.validation);
         const { error, value } = validation.validate(req.body, Validator.options);
-
+        
         if (error) res.json({ message: "Validation Error" });
         else {
-            const user = await new User(value);
-            user.mobileNumber = value.mobileNumber.substring(value.mobileNumber.length - 11);
-            //console.log('user = ', user);
-            user.save().then(result => {
+            const num = value.mobileNumber.substring(value.mobileNumber.length - 11)
+            const DBNumber = await User.findOne({ mobileNumber: num });
+
+            if (DBNumber) {
+                res.status(202).json({
+                   message: "Number is already use."
+                });
+            } else {
+              const user = await new User(value);
+              user.mobileNumber = value.mobileNumber.substring(
+                value.mobileNumber.length - 11
+              );
+              const result = await user.save();
+              if (result) {
                 res.status(201).json({
                   message: "User Created Successfully",
-                  userDetails: result
+                  userDetails: result,
                 });
-              })
-              .catch((err) => {
-                res.status(401).json(err);
-              });
+              }
+            }
         }
+    }
+    catch (err) {
+        res.status(400).json(err);
+    } 
+};
+
+
+// Update User 
+async function updateUser(req, res) {
+    try {
+      // validation check
+      const validation = Joi.object(Validator.validation);
+      const { error, value } = validation.validate(req.body, Validator.options);
+
+      if (error) res.json({ message: "Validation Error" });
+      else {
+        const result = await User.findByIdAndUpdate({ _id: req.params.id }, { $set: {
+            name: value.name,
+            mobileNumber: value.mobileNumber.substring(value.mobileNumber.length - 11)}
+        });
+        if (result) {
+          res.status(200).json({
+            message: "Update user",
+            name: value.name,
+            mobileNumber: value.mobileNumber,
+          });
+        }
+      }
     }
     catch (err) {
         res.status(400).json(err);
@@ -80,52 +116,21 @@ async function createUser(req, res) {
 };
 
 
-// Update User 
-const updateUser = (req, res) => {
-    const validation = Joi.object(Validator.validation);
-    const { error, value } = validation.validate(req.body, Validator.options);
-
-    if (error) res.json({ message: "Validation Error" });
-    else {
-        console.log('ID = ', req.params.id);
-        User.findByIdAndUpdate({ _id: req.params.id }, {
-            $set: {
-                'name': value.name,
-                'mobileNumber': value.mobileNumber.substring(value.mobileNumber.length - 11)
-            }
-        }).exec().then(() => {
-            res.status(200).json({
-                message: "Update user",
-                name: value.name,
-                mobileNumber: value.mobileNumber
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: "Error, Please try again.",
-                error: err.message
-            });
-        });
-    }
-};
-
-
 
 // Delete User 
-const deleteUser = (req, res) => {
-    const id = req.params.id;
-    User.findByIdAndRemove({ _id: id })
-        .then(() => {
-            res.status(204).json({
-                message: 'User delete Successfully.'
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: "Error, Please try again.",
-                error: err.message,
-            });
+async function deleteUser(req, res) {
+    try {
+      const id = req.params.id;
+      const result = await User.findByIdAndRemove({ _id: id });
+      if (result) {
+        res.status(204).json({
+          message: "User delete Successfully.",
         });
+      }
+    }
+    catch (err) {
+      res.status(400).json(err);
+    }
 };
 
 
